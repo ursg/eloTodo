e62db769915584da025d74681086507b736b6acf
Urs Ganse <urs.ganse@helsinki.fi>
2025-12-16 10:58:43 +0200

    Add github compilation action
    Add github compilation action

--- make.lisp
+++ make.lisp
@@ -1,6 +1,13 @@
 (require :asdf)
 (require :uiop)
 
+;; Download & install Quicklisp in the script rather than the GitHub Actions
+;; because it's easier; the downloaded file is already in our working directory.
+(uiop:run-program (list "curl" "-O" "https://beta.quicklisp.org/quicklisp.lisp"))
+(load "quicklisp.lisp")
+(quicklisp-quickstart:install)
+
 (push (concatenate 'string (namestring (uiop:getcwd)) "/") asdf:*central-registry*)
 
+(ql:quickload :elotodo)
 (asdf:make :elotodo)
